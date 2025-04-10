f2637579fca2e3762930fe98c8d0ca5f2ed4b73f
inakicarcereny <inakicarce@hotmail.com.ar>
2025-04-10 19:18:58 -0300

    Add Prettier and ESLint configuration files; set up .prettierignore for optimized formatting and linting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,21 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+    plugins: ['unused-imports', 'react', 'react-hooks', 'jsx-a11y', 'import'],
+    rules: {
+      semi: 'error',
+      quotes: ['error', 'single'],
+      'prefer-arrow-callback': 'error',
+      'unused-imports/no-unused-imports': 'error',
+
+      '@next/next/no-img-element': 'error',
+      '@next/next/no-sync-scripts': 'error',
+      '@next/next/no-css-tags': 'error',
+      '@next/next/no-title-in-document-head': 'error',
+    },
+  }),
 ];
 
 export default eslintConfig;

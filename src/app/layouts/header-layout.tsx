39715c6fa8c32e12efa3e818a9afac2ecaec9a09
inakicarcereny <inakicarce@hotmail.com.ar>
2025-04-19 13:38:58 -0300

    Enhance HeaderLayout with search functionality, task creation, and theme toggle buttons for improved user experience

--- src/app/layouts/header-layout.tsx
+++ src/app/layouts/header-layout.tsx
@@ -1,3 +1,6 @@
+import { CreateTaskButton } from '@/components/CreateTaskButton';
+import { ToggleThemeButton } from '@/components/ToggleThemeButton';
+import { Search } from '@/icons/Search';
 import { JSX, ReactNode } from 'react';
 
 interface HeaderLayoutProps {
@@ -6,11 +9,32 @@ interface HeaderLayoutProps {
 
 export function HeaderLayout({ children }: HeaderLayoutProps): JSX.Element {
   return (
-    <div>
-      <header>
-        <h1>Header layout</h1>
+    <>
+      <header className='w-full border-b bg-[#1e1f1f]'>
+        <div className='mx-auto flex h-[80px] max-w-[1252px] items-center justify-between'>
+          <h1 className='text-2xl font-semibold text-white'>Tasks</h1>
+
+          <div className='flex items-center gap-4'>
+            <div className='flex max-w-[250px] items-center gap-2 rounded-md bg-white px-4 py-2'>
+              <input
+                type='text'
+                className='w-full border-none bg-transparent focus:outline-none'
+                placeholder='Search...'
+              />
+
+              <span>
+                <Search />
+              </span>
+            </div>
+
+            <CreateTaskButton />
+
+            <ToggleThemeButton />
+          </div>
+        </div>
       </header>
-      <div>{children}</div>
-    </div>
+
+      <main className='mx-auto h-full max-w-[1252px] py-4'>{children}</main>
+    </>
   );
 }

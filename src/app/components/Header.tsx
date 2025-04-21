23a64d95c6917bb228804a51fbdf6dafdc7b764b
inakicarcereny <inakicarce@hotmail.com.ar>
2025-04-20 22:05:51 -0300

    .

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,5 @@
 import { JSX } from 'react';
 
 export function Header(): JSX.Element {
-  return (
-    <header className='mx-auto flex h-[80px] w-full items-center justify-between border-b bg-[#1e1f1f]'>
-      <h1 className='text-2xl font-semibold text-white'>Tasks</h1>
-    </header>
-  );
+  return <div>Not used at the moment.</div>;
 }

bf40c8e45db1e12689a696cc0be139355db6217e
inakicarcereny <inakicarce@hotmail.com.ar>
2025-04-22 21:21:41 -0300

    Refactor CreateTaskButton to open modal for task creation instead of logging to console

--- src/app/components/CreateTaskButton.tsx
+++ src/app/components/CreateTaskButton.tsx
@@ -2,12 +2,15 @@
 
 import { Button } from '@/components/ui/Button';
 import { Plus } from '@/icons/Plus';
+import { useModalStore } from '@/store/modal-store';
 import { JSX } from 'react';
 
 export function CreateTaskButton(): JSX.Element {
+  const { openModal } = useModalStore();
+
   return (
     <Button
-      onClick={() => console.log('create')}
+      onClick={() => openModal('create-task')}
       icon={<Plus />}
       variant='secondary'
     >

210b9240a26cd811cac0c9c872414050b9b07492
inakicarcereny <inakicarce@hotmail.com.ar>
2025-04-26 18:03:12 -0300

    Update header layout styles: change header background and text colors for improved visibility, adjust search input styles for better UX

--- src/app/layouts/header-layout.tsx
+++ src/app/layouts/header-layout.tsx
@@ -10,20 +10,20 @@ interface HeaderLayoutProps {
 export function HeaderLayout({ children }: HeaderLayoutProps): JSX.Element {
   return (
     <>
-      <header className='w-full border-b bg-[#1e1f1f]'>
+      <header className='w-full bg-white'>
         <div className='mx-auto flex h-[80px] max-w-[1252px] items-center justify-between'>
-          <h1 className='text-2xl font-semibold text-white'>Tasks</h1>
+          <h1 className='text-2xl font-semibold text-black'>Tasks</h1>
 
           <div className='flex items-center gap-4'>
-            <div className='flex max-w-[250px] items-center gap-2 rounded-md bg-white px-4 py-2'>
+            <div className='flex max-w-[250px] items-center gap-2 rounded-md bg-zinc-200 px-4 py-2'>
               <input
                 type='text'
-                className='w-full border-none bg-transparent focus:outline-none'
+                className='w-full border-none bg-transparent placeholder:text-zinc-400 focus:outline-none'
                 placeholder='Search...'
               />
 
               <span>
-                <Search />
+                <Search className='text-zinc-400' />
               </span>
             </div>
 
@@ -34,7 +34,7 @@ export function HeaderLayout({ children }: HeaderLayoutProps): JSX.Element {
         </div>
       </header>
 
-      <main className='mx-auto flex h-full max-w-[1252px] flex-col py-4'>
+      <main className='mx-auto flex h-full w-full max-w-[1252px] flex-col py-4'>
         {children}
       </main>
     </>

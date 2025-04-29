9ab1ec515bc8b2969b7fd3d2f2056ecdf369f603
inakicarcereny <inakicarce@hotmail.com.ar>
2025-04-29 15:16:20 -0300

    Refactor task management functions to use TaskId and UpdateTask types for better type safety in create, update, and delete operations

--- src/app/actions/task.ts
+++ src/app/actions/task.ts
@@ -1,8 +1,8 @@
 'use server';
 
 import { prisma } from '@/lib/prisma';
-import { Task } from '@/types/task';
-import { CreateTask } from '@/schemas/task';
+import { Task, TaskId } from '@/types/task';
+import { CreateTask, UpdateTask } from '@/schemas/task';
 import { revalidatePath } from 'next/cache';
 
 export async function getTasks(): Promise<Task[]> {
@@ -21,7 +21,7 @@ export async function createTask(task: CreateTask): Promise<Task> {
   return newTask;
 }
 
-export async function updateTask(id: string, task: CreateTask): Promise<Task> {
+export async function updateTask(id: TaskId, task: UpdateTask): Promise<Task> {
   const updatedTask = await prisma.task.update({
     data: task,
     where: {
@@ -34,7 +34,7 @@ export async function updateTask(id: string, task: CreateTask): Promise<Task> {
   return updatedTask;
 }
 
-export async function deleteTask(id: string): Promise<Task> {
+export async function deleteTask(id: TaskId): Promise<Task> {
   const deletedTask = await prisma.task.delete({
     where: {
       id,

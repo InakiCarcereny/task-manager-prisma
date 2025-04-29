8927011c302660dbef99f33372a12dde0527e153
inakicarcereny <inakicarce@hotmail.com.ar>
2025-04-29 15:16:31 -0300

    Add cursor-pointer style to Button component for improved user interaction feedback

--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -3,7 +3,7 @@ import { cva } from 'class-variance-authority';
 import { JSX } from 'react';
 
 const buttonVariants = cva(
-  'flex items-center justify-center rounded-md font-semibold gap-2 px-4 py-2',
+  'flex items-center justify-center rounded-md font-semibold gap-2 px-4 py-2 cursor-pointer',
   {
     variants: {
       variant: {

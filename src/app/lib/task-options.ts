4bc6e9241a26b8a022be44a6b11aee7201e5441e
inakicarcereny <inakicarce@hotmail.com.ar>
2025-04-29 15:15:16 -0300

    Add TASK_OPTIONS in task-options.ts with edit and delete functionality for task management in the application

---
+++ src/app/lib/task-options.ts
@@ -0,0 +1,23 @@
+import { deleteTask } from '@/actions/task';
+import { Pencil } from '@/icons/Pencil';
+import { Trash } from '@/icons/Trash';
+import { Task } from '@/types/task';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
+
+export const TASK_OPTIONS = [
+  {
+    id: 1,
+    icon: Pencil({}),
+    bg: 'bg-red-100',
+    text: 'text-red-400',
+    onClick: (taskId: string, router: AppRouterInstance): void =>
+      router.push(`/task/${taskId}`),
+  },
+  {
+    id: 2,
+    icon: Trash({}),
+    bg: 'bg-zinc-100',
+    text: 'text-zinc-400',
+    onClick: (taskId: string): Promise<Task> => deleteTask(taskId),
+  },
+] as const;
